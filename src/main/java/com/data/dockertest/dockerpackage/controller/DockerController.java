9c05470c5fd0d9918c29ea1d240e382287faff4c
baijiliang <497807178@qq.com>
2020-01-07 13:20:11 +0800

    测试springboot数据校验

--- src/main/java/com/data/dockertest/dockerpackage/controller/DockerController.java
+++ src/main/java/com/data/dockertest/dockerpackage/controller/DockerController.java
@@ -1,8 +1,6 @@
 package com.data.dockertest.dockerpackage.controller;
 
 import com.data.dockertest.dockerpackage.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +9,6 @@ import javax.validation.Valid;
 
 @RestController
 public class DockerController {
-    @Autowired
-    private RedisTemplate redisTemplate;
     @RequestMapping("/docker")
     public String getMessage() {
         return "Hello Docker";
